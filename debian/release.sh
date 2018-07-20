e55cfad72d60c2cc9d7dd21483ec023dadc5fc2e
David Lechner <david@lechnology.com>
2018-07-20 17:09:25 -0500

    have to build on armhf to get ev3dev version of connman

--- debian/release.sh
+++ debian/release.sh
@@ -9,9 +9,9 @@ version=$(dpkg-parsechangelog -S Version)
 distribution=$(dpkg-parsechangelog -S Distribution)
 codename=$(debian-distro-info --codename --${distribution})
 
-OS=debian DIST=${codename} ARCH=amd64 pbuilder-ev3dev build
-debsign ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
+OS=debian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
+debsign ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
 
 ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedsc ${codename} \
     ~/reprepro/debian/pool/main/${source:0:1}/${source}/${source}_${version}.dsc"
